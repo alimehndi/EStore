8e373e2d0f640712903a80002847c94770f09b31
alimehndi <alimehndi99@gmail.com>
2023-08-27 02:47:44 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+      <h1>Welcome to Eshop</h1>
+      </Container>
+    </main>
+    </>
+
+    )
 }
 
 export default App
